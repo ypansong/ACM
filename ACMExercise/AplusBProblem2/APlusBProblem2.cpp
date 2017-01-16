e7a95260bd5afaf77c7191e4136211d4453b5e96
YongpanSong <735830492@qq.com>
2017-01-16 21:29:14 +0800

    add A+B Problem 2

--- ACMExercise/AplusBProblem2/APlusBProblem2.cpp
+++ ACMExercise/AplusBProblem2/APlusBProblem2.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+string add(string a, string b)
+{
+    const char *a_array = a.c_str();
+    int a_len = a.length();
+
+    const char *b_array = b.c_str();
+    int b_len = b.length();
+
+    char sum[2000] = { 0 };
+
+
+    return string(sum);
+}
 
 int main()
 {
     int test_case_num = 0;
+
     while (cin >> test_case_num)
     {
-        long long *a = new long long[test_case_num];
-        long long *b = new long long[test_case_num];
+        string *a = new string[test_case_num];
+        string *b = new string[test_case_num];
+        
         for (int i = 0; i < test_case_num; i++)
         {
             cin >> a[i] >> b[i];
         }
+
         for (int i = 0; i < test_case_num; i++)
         {
             cout << "Case " << i + 1 << ":" << endl;
-            cout << a[i] << " + " << b[i] << " = " << a[i] + b[i] << endl;
+            cout << a[i] << " + " << b[i] << " = " << add(a[i], b[i]) << endl;
         }
-        delete[]a;
-        delete[]b;
     }
 
     return 0;
